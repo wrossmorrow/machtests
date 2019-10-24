c9feede60be9e5035675321ad46fb3314b94b346
W. Ross Morrow <wrossmorrow@stanford.edu>
2019-10-24 07:08:24 -0700

    working on changes in a dev branch

--- test.sh
+++ test.sh
@@ -259,6 +259,7 @@ function testScript() {
 		log "cleaning environment..."
 
 		# IMPORTANT!! unset any YENTESTS_ vars to run the next test suite "clean"
+		env | grep '^YENTESTS_'
 		env | grep '^YENTESTS_' | xargs -i 'unset {}'
 
 		# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
