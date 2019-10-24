01aa7042841e772363d99a9acce909b787dbf11f
W. Ross Morrow <wrossmorrow@stanford.edu>
2019-10-24 15:29:26 -0700

    trying to handle error data better

--- checktests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#query the yentests records
-
-script_home=$( dirname $(realpath 0$) )
-test_db=yentests.db
-db_folder=$script_home/db_scripts
-test_db_file="$db_folder/$test_db"
-sqlite3 $test_db_file ".read $db_folder/checktests.sql"
